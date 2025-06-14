053956830a9fa4a375e60c3adec4c11103ffefc8
Muhammad Muneeb Ur Rehman <m.muneeburrehmann@gmail.com>
2025-06-14 14:16:21 +0500

    Main Done

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.portfolio"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13599879"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
